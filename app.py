5bad47f072e06c328a70a039d551142c9640e2f6
omega-quant <seemaupadhyay2505@gmail.com>
2026-04-29 18:47:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,11 +55,3 @@ if __name__ == '__main__':
 def home():
     return render_template('index.html')
 
-import os
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
-@app.route('/test')
-def test():
-    return "Hello from backend"
