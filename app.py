b0efb734aa7c6524b66661bd3b4bcd827241b384
omega-quant <seemaupadhyay2505@gmail.com>
2026-04-29 18:06:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+# DB setup
+def init_db():
+    conn = sqlite3.connect('quotes.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS quotes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Get all quotes
+@app.route('/get_quotes')
+def get_quotes():
+    conn = sqlite3.connect('quotes.db')
+    c = conn.cursor()
+    c.execute("SELECT text FROM quotes")
+    data = c.fetchall()
+    conn.close()
+
+    return jsonify([q[0] for q in data])
+
+# Add quote
+@app.route('/add_quote', methods=['POST'])
+def add_quote():
+    data = request.json
+    text = data.get('text')
+
+    conn = sqlite3.connect('quotes.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO quotes (text) VALUES (?)", (text,))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Quote added"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
